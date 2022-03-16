4440009bd240aebb6f049bf166baea60d7ea894b
Minho Yu <ymh0770@gmail.com>
2022-03-16 20:33:34 +0900

    add view,url

--- mountain/models.py
+++ mountain/models.py
@@ -6,9 +6,16 @@ class Mountain(models.Model):
     mountain_name = models.CharField(max_length=30, blank=True, null=True)
     subtitle = models.TextField(blank=True, null=True)
     height = models.IntegerField(blank=True, null=True)
+    selection_reason = models.TextField(blank=True, null=True)
     location = models.CharField(max_length=300, blank=True, null=True)
-    information = models.TextField(blank=True, null=True)
-    location_code = models.FloatField(blank=True, null=True)
+    detail = models.TextField(blank=True, null=True)
+    course_information = models.TextField(blank=True, null=True)
+    mountain_information = models.TextField(blank=True, null=True)
+    stay = models.TextField(blank=True, null=True)
+    transportation = models.TextField(blank=True, null=True)
+    local_code = models.FloatField(blank=True, null=True)
+    local_name = models.TextField(blank=True, null=True)
+
 
     class Meta:
         managed = True
--- mountain/views.py
+++ mountain/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Mountain
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+# @login_required()
+def mountains(request):
+    # user = request.user.is_authenticated
+    all_mountain = Mountain.objects.all()
+    return render(request, 'mountain/all_mountain.html', {'mountains':all_mountain})
+
+def mountains_detail(request, id):
+    my_mountain = Mountain.objects.get(id=id)
+    return render(request, 'mountain/my_mountain.html', {'mountain_info':my_mountain})
