71db35724387de496bdb31e88b9e33f488b3b0ab
Minho Yu <ymh0770@gmail.com>
2022-03-18 15:56:43 +0900

    post/urls.py

--- post/urls.py
+++ post/urls.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.urls import path
 from django.conf.urls.static import static
 
-from . import post_service, comment_service
+from . import post_service
 
 urlpatterns = [
     path('posts/', post_service.post_list, name='posts'),
     path('posts/<int:pk>/', post_service.post_detail, name='posts-detail'),
     path('posts/new/', post_service.post, name='posts-new'),
-    path('posts/<int:pk>/comments/', comment_service.comment, name='comment'),
+    # path('posts/<int:pk>/comments/', comment_service.comment, name='comment'),
 ]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
