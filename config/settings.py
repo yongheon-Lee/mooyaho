69d5b0e5045d53562dade8b3d9ff5a6ad82c10f7
jungjaechoi <joong8812@gmail.com>
2022-03-22 10:47:46 +0900

    fix - eb 올리기 위해 debug, hosts 설정 변경

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = MY_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 # 장고 기본 설정 app 추가
