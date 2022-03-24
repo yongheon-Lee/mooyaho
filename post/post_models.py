31bf0ba72ed88de549c07788570a1b9c805a5afa
yongheon-Lee <dydgjs778@gmail.com>
2022-03-24 11:40:17 +0900

    수정: 포스팅 이미지 파일명 설정 코드 수정

--- post/post_models.py
+++ post/post_models.py
@@ -18,7 +18,7 @@ def hiking_img_upload_path(instance, filename):
     # 확장자 추출
     extension = os.path.splitext(filename)[-1].lower()
     # 파일명 설정
-    custom_file_name = '/'.join([prefix, uuid_name, extension])
+    custom_file_name = f"{prefix}/{uuid_name}/posting{extension}"
     return custom_file_name
 
 
