1fc38b963076764388fa87afbaf68f6b1bd3e3d6
jungjaechoi <joong8812@gmail.com>
2022-03-28 15:54:14 +0900

    add - [프론트-로그인] 공백체크, 엔터 로그인 시도 구현 #99

--- user/static/js/user/login.js
+++ user/static/js/user/login.js
@@ -1,3 +1,4 @@
+// csrf 토큰을 얻음
 const getCsrfTokenElement = () => {
     const csrfToken = document.getElementsByName('csrfmiddlewaretoken')[0];
     const hiddenField = document.createElement("input");
@@ -7,6 +8,7 @@ const getCsrfTokenElement = () => {
     return hiddenField;
 }
 
+// 동적 form(로그인) 요청
 const submitDynamicForm = (loginInfo) => {
     const newForm = document.createElement('form');
 
@@ -27,20 +29,28 @@ const submitDynamicForm = (loginInfo) => {
     newForm.submit();
 }
 
+// 로그인 요청
+const requestLogin = () => {
+    const inputInfo = {
+        'email': document.querySelector('#email').value,
+        'password': document.querySelector('#userPw').value,
+        'latitude': latitude,
+        'longitude': longitude,
+    }
+    submitDynamicForm(inputInfo);
+}
+
+// 로그인 or 회원가입 버튼 눌렀을 때
 const clickedButton = (e) => {
     if (e.target.innerText == '로그인') {
-        const inputInfo = {
-            'email': document.querySelector('#email').value,
-            'password': document.querySelector('#userPw').value,
-            'latitude': latitude,
-            'longitude': longitude,
-        }
-        submitDynamicForm(inputInfo);
+        if (!checkBlankLoginInfo()) return;
+        requestLogin();
     } else {
         location.href = '/signup';
     }
 }
 
+// 사용자 위치정보를 얻음
 const getUserLocation = () => {
     if (!navigator.geolocation) {
         throw "위치 정보가 지원되지 않습니다.";
@@ -49,6 +59,30 @@ const getUserLocation = () => {
         latitude = coords.latitude // 위도
         longitude = coords.longitude; // 경도
     });
+}
+
+// 로그인 정보(이메일, 비밀번호) 공백인지 확인
+const checkBlankLoginInfo = () => {
+    const loginInfo = [
+        document.querySelector('#email'),
+        document.querySelector('#userPw')
+    ]
+    for (let index in loginInfo) {
+        if (loginInfo[index].value == '') {
+            document.querySelector('#error-msg-area > span').innerText = '이메일 혹은 비밀번호가 공백입니다.'
+            loginInfo[index].focus();
+            return false;
+        }
+    }
+    return true;
+}
+
+// 키보드를 눌렀을 때
+const afterKeyDown = (e) => {
+    if (e.keyCode == 13) { // enter
+        if (!checkBlankLoginInfo()) return;
+        requestLogin();
+    }
 }
 
 // 실행 부분
@@ -61,3 +95,11 @@ for (let i=0; i<buttons.length; i++){
 
 // 유저 위치 정보
 getUserLocation();
+
+// keyboard event
+window.addEventListener('keydown', afterKeyDown);
+
+// DOM 구성 후 
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector('#email').focus();
+});
