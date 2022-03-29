ec765141595d2ced013ff68312ff597d8051e67f
hojin <hojin9car@gmail.com>
2022-03-30 03:42:28 +0900

    help 앱 작업 중간저장 notice 수정, 삭제 review 수정, 삭제

--- help/urls.py
+++ help/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
     path('help/', views.index, name='help'),
     path('help/notice/post_notice', views.post_notice, name='post_notice'),
     path('help/notice/', views.notice, name='notice'),
+    path('help/notice/edit/<int:id>', views.edit_notice, name='edit_notice'),
     path('noice/delete_notice/<int:id>', views.delete_notice, name="delete_notice"),
     path('help/review/', views.review, name='review'),
     path('help/review/new', views.post_review, name='post_review'),
+    path('help/review/delete/<int:id>', views.delete_review, name='del_review'),
+    path('help/review/update/<int:id>', views.update_review, name='update-review'),
+
 ]
