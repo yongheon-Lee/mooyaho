ab7d3a170853fa8cf78713119639aabc23a66c5d
jungjaechoi <joong8812@gmail.com>
2022-03-29 10:31:26 +0900

    remove - 사용하지 않지 않는 my_settings 호출 삭제 #107

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os.path
 from pathlib import Path
-from .my_settings import MY_SECRET, MY_DATABASES, MY_AWS
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = MY_SECRET
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -90,7 +88,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = MY_DATABASES
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE'),
