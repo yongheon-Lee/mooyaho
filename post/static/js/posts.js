bb22133dfb3b7aca24bd878a53763d82d4707791
jungjaechoi <joong8812@gmail.com>
2022-03-30 14:38:20 +0900

    fix - [프론트-헤더] 글쓰기 버튼 아이콘으로 변경 #132

--- post/static/js/posts.js
+++ post/static/js/posts.js
@@ -6,7 +6,12 @@ function changeSearchToAdd() {
     // img = document.getElementsByClassName("meterial-icons");
     const img1 = document.getElementById("search-wrapper");
     img1.removeEventListener('click', clickedSearchButton);
-    img1.innerHTML = `<button type="button" onclick="location.href='/posts/new'">글쓰기</button>`;
+    img1.innerHTML = `<div onclick="location.href='/posts/new'"
+        style="color: black;">
+        <span class="material-icons" style="font-size: 2rem;">
+        create
+        </span>
+    </div>`;
 }
 
 function modalReport() {
