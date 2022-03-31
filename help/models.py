b4feb44d410f4ffd80d961c2a871a1014e2f67cb
yongheon-Lee <dydgjs778@gmail.com>
2022-03-31 11:04:49 +0900

    수정: 각 모델이 관리자 페이지에서 표시되는 형식 수정

--- help/models.py
+++ help/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from user.user_models import MooyahoUser
 from post.post_models import Post
 
+
 # Create your models here.
 class Notice(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False)
@@ -10,8 +11,13 @@ class Notice(models.Model):
     create_at = models.DateField(auto_now_add=True)
     deleted = models.BooleanField(default=False)
 
-    class Meta :
+    class Meta:
         db_table = 'notice'
+
+    # 각 객체가 공지사항 제목으로 표시되도록 설정
+    def __str__(self):
+        return self.title
+
 
 class Review(models.Model):
     author = models.ForeignKey(MooyahoUser, on_delete=models.CASCADE, db_column='author')
@@ -23,5 +29,11 @@ class Review(models.Model):
     create_at = models.DateField(auto_now_add=True)
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE, db_column='post_id', null=True)
 
-    class Meta :
+    class Meta:
         db_table = 'review'
+
+    # 각 객체가 글 작성자, 내용 일부로 표시되도록 설정
+    def __str__(self):
+        author_nickname = self.author.nickname
+        part_of_content = self.content[:5]
+        return author_nickname + ' / ' + part_of_content + '...'
