c9bf43c69333d1586aa31b1e14e999b6d982fee1
hojin <hojin9car@gmail.com>
2022-04-08 15:39:19 +0900

    convert jpeg image files to webp

---
+++ mountain/static/images/mountains/webp.py
@@ -0,0 +1,10 @@
+# pip install Pillow
+
+import os
+from PIL import Image
+
+# for file in os.listdir():
+#     if file.endswith(".jpeg"):
+#         print(file[:-5])
+#         im = Image.open(file).convert('RGB')
+#         im.save(file[:-5] + '.webp', 'webp')
